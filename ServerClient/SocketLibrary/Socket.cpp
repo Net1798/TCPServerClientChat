bcbd6936fe756e5013be6c05fe5759fca66daf56
Net1798 <53884182+Net1798@users.noreply.github.com>
2024-11-18 17:19:16 -0500

    Add files via upload

---
+++ ServerClient/SocketLibrary/Socket.cpp
@@ -0,0 +1,54 @@
+#include "Socket.h"
+
+
+//class Socket
+
+Socket::Socket()
+{
+
+}
+
+
+Socket::~Socket()
+{
+
+}
+
+void Socket::closeSocket()
+{
+
+}
+
+
+//class serverSocket
+void ServerSocket::bindConnection(int port)
+{
+
+}
+
+
+void ServerSocket::listenConnection(int backlog)
+{
+
+}
+
+
+
+void ServerSocket::acceptConnection()
+{
+
+}
+
+//class clientSocket
+
+void ClientSocket::connectToServer(std::string& host, int port)
+{
+
+}
+
+
+void ClientSocket::sendMessage(std::string& message)
+{
+
+}
+
